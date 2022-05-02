36156f6e0ee6625ffee07e7bcb402f19ef14315b
Breno Santos <brenosantos145@gmail.com>
2022-05-02 20:56:07 -0300

    tá indo

---
+++ index.js
@@ -0,0 +1,24 @@
+import puppeteer from 'puppeteer';
+
+const URL =
+  'https://be-trybe.typeform.com/to/NXygjr?typeform-source=playbook.betrybe.com#cohort_name=Turma%2017';
+
+const textArray = ['Bot bonito', 'Meireles é feio', 'Cabeça de bola de bilhar'];
+
+const type = async (page, text) => {
+  await page.waitForTimeout(1000);
+  await page.keyboard.type(text);
+  await page.keyboard.press('Enter');
+};
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(URL);
+  await page.keyboard.press('Enter');
+  await page.keyboard.press('Enter');
+  for (let text of textArray) {
+    await type(page, text);
+  }
+  await browser.close();
+})();
